f8e4957916798bf0f71cc5d9a09e0a4e7f4d9415
Sree Devi <sreedevi.proffid@gmail.com>
2025-05-29 15:01:10 +0530

    fourth commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
-import SignIn from './pages/Signin'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
